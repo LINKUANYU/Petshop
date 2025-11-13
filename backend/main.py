7b0ceed5ba22bd41ba87e17dc1e6536b22796481
kaun <kuan@gmail.com>
2025-11-13 15:43:50 +0800

    完成商品列表分頁

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,11 @@ templates = Jinja2Templates(directory=TEMPLATES_DIR)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.get("/daily_discover")
+def home(request: Request):
+    return templates.TemplateResponse("daily.html", {"request": request})
+
+
 app.mount("/frontend", StaticFiles(directory=FRONTEND_DIR), name = "frontend")
 app.mount("/static", StaticFiles(directory=BACKEND_STATIC), name = "backend-static")
 
@@ -35,46 +40,32 @@ def get_conn():
 
 # API 
 
+PAGE_SIZE = 5
+
 # 型別設定
-@app.get("/api/products")
-def list_product(
-    page: int = Query(1, ge=1),
-    page_size: int = Query(5, ge=1, le=5),
-    q: str | None = Query(None)
+@app.get("/api/daily-discover/highlights")
+def daily_discover_highlights(
+    limit: int = Query(5, ge=1, le=5),
 ):
 
     # 設定篩選條件
-    offset_data = (page - 1) * page_size
     condition = ["p.is_active = 1", "v.is_active = 1"]
-    params: list = []
-
-    if q:
-        condition.append("p.name LIKE %s")
-        params.append(f'%{q}%')
-
     where_sql = " AND ".join(condition)
-    # 統計data總數
-    count_sql = f'''
-        SELECT COUNT(DISTINCT p.id) AS cnt FROM products p JOIN product_variants v
-        ON p.id = v.product_id
-        WHERE {where_sql}
-    '''
 
     # 商品資料
-    data_sql = f'''
+    sql = f'''
     SELECT p.id, p.name, p.main_image, MIN(v.price) AS price_cents
     FROM products p JOIN product_variants v ON p.id = v.product_id
-    WHERE {where_sql} GROUP BY p.id, p.name, p.main_image 
-    ORDER BY p.id LIMIT %s OFFSET %s
+    WHERE {where_sql} 
+    GROUP BY p.id, p.name, p.main_image 
+    ORDER BY p.created_at DESC
+    LIMIT %s 
     '''
     # 執行sql
     conn = get_conn()
     cur = conn.cursor(dictionary=True)
 
-    cur.execute(count_sql, params)
-    total = cur.fetchone()["cnt"]
-
-    cur.execute(data_sql, params + [page_size, offset_data])
+    cur.execute(sql, (limit,))
     rows = cur.fetchall()
 
     cur.close()
@@ -87,4 +78,67 @@ def list_product(
         "img": r["main_image"],
         "price": r["price_cents"] // 100 if r["price_cents"] is not None else None}
         for r in rows]
-    return {"data": data, "page": page, "page_size": page_size, "total": total}
+    return {"data": data, "count": len(data)}
+
+
+@app.get("/api/daily-discover")
+def daily_discover_highlights(
+    pageNumber: int = Query(1, ge=1),
+):
+
+    # 設定篩選條件
+    condition = ["p.is_active = 1", "v.is_active = 1"]
+    where_sql = " AND ".join(condition)
+
+    #算總數 
+    total_sql = f'''
+    SELECT COUNT(DISTINCT p.id) AS counts
+    FROM products p JOIN product_variants v ON p.id = v.product_id
+    WHERE {where_sql}
+    '''
+
+    # OFFSET
+    offset = (pageNumber - 1) * PAGE_SIZE
+
+    # 商品資料
+    sql = f'''
+    SELECT p.id, p.name, p.main_image, MIN(v.price) AS price_cents
+    FROM products p JOIN product_variants v ON p.id = v.product_id
+    WHERE {where_sql} 
+    GROUP BY p.id, p.name, p.main_image 
+    ORDER BY p.created_at DESC
+    LIMIT %s OFFSET %s
+    '''
+
+    # 執行sql
+    conn = get_conn()
+    cur = conn.cursor(dictionary=True)
+
+    cur.execute(total_sql)
+    total = cur.fetchone()["counts"]
+    
+    cur.execute(sql, (PAGE_SIZE, offset))
+    rows = cur.fetchall()
+
+    has_next = (pageNumber * PAGE_SIZE) < total
+    next_pageNumber = pageNumber + 1 if has_next else None
+
+
+    cur.close()
+    conn.close()
+
+    # 建立回傳json
+    data = [{
+        "id": r["id"],
+        "name": r["name"],
+        "img": r["main_image"],
+        "price": r["price_cents"] // 100 if r["price_cents"] is not None else None}
+        for r in rows]
+    return {
+        "data": data,
+        "pageNumber": pageNumber,
+        "page_size": PAGE_SIZE,
+        "total":total,
+        "hasNext": has_next,
+        "next_pageNumber": next_pageNumber
+        }
